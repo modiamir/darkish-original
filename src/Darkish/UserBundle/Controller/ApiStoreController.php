f89876bbbebf7c75506fe6652d8676d6a2b4b343
Amir Modarresi <amir.modarresi.13@gmail.com>
2015-08-02 17:08:44 +0430

    Missing Commit Message

--- src/Darkish/UserBundle/Controller/ApiStoreController.php
+++ src/Darkish/UserBundle/Controller/ApiStoreController.php
@@ -7,6 +7,7 @@ use Darkish\CategoryBundle\Entity\StoreGroup;
 use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations\Route;
 use FOS\RestBundle\Controller\Annotations\View;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\BrowserKit\Response;
@@ -31,6 +32,7 @@ class ApiStoreController extends Controller
      * @return array|\Darkish\CategoryBundle\Entity\Product[]
      * @Get("/get_store_products/{record}", name="_api_store")
      * @View(serializerGroups={"api.list", "file.details", "api.store"})
+     * @ApiDoc()
      */
     public function getStoreProductsAction(Record $record) {
         $products = $this->getDoctrine()->getRepository('DarkishCategoryBundle:Product')
@@ -46,6 +48,7 @@ class ApiStoreController extends Controller
      * @return array|\Darkish\CategoryBundle\Entity\Product[]
      * @Get("/get_store_products_per_group/{storeGroup}", name="_api_store")
      * @View(serializerGroups={"api.list", "file.details", "api.store"})
+     * @ApiDoc()
      */
     public function getStoreProductsPerGroupAction(StoreGroup $storeGroup) {
         $products = $this->getDoctrine()->getRepository('DarkishCategoryBundle:Product')
