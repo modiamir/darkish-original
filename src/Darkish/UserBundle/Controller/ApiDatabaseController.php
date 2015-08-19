783385d9e1207ad1dd7684176969ee643bb04c68
Amir Modarresi <amir.modarresi.13@gmail.com>
2015-08-19 11:03:00 +0430

    Missing Commit Message

--- src/Darkish/UserBundle/Controller/ApiDatabaseController.php
+++ src/Darkish/UserBundle/Controller/ApiDatabaseController.php
@@ -14,6 +14,7 @@ use FOS\RestBundle\Controller\Annotations\View;
 use JMS\Serializer\SerializationContext;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -26,7 +27,10 @@ class ApiDatabaseController extends Controller
     /**
      * @Post("/search_database/{mode}/{modevalue}", name="_api_database", defaults={"_format"= "json"},
      * requirements={"mode": "global|record"})
-     * @ApiDoc()
+     * @ApiDoc(
+     * resource=true,
+     *  section="Database API"
+     * )
      * @View(serializerGroups={"api.list"})
      */
     public function searchDatabaseAction($mode, $modevalue= null, Request $request = null)
@@ -76,16 +80,94 @@ class ApiDatabaseController extends Controller
 
         $form->handleRequest($this->container->get('request'));
 
+        $prices = [];
+        if($request->request->has('priceFrom'))
+        {
+            $prices['from'] = $request->get('priceFrom');
+        }
+
+        if($request->request->has('priceTo'))
+        {
+            $prices['to'] = $request->get('priceTo');
+        }
+
+
+
         if($mode== 'global')
         {
-            $query = $repo->search($database);
+            $query = $repo->search($database, $prices);
         } elseif($mode== 'record')
         {
-            $query = $repo->search($database, $record);
+            $query = $repo->search($database, $prices, $record);
         }
 
 
 
         return new Response($this->get('jms_serializer')->serialize($query->getResult(), 'json', SerializationContext::create()->setGroups(['api.details', 'api.list', 'file.details', 'customer.list'])));
+    }
+
+    /**
+     * @Post("/get_estate_types", name="_api_database_get_estate_types", defaults={"_format"= "json"},
+     * requirements={"mode": "global|record"})
+     * @ApiDoc(
+     * resource=true,
+     *  section="Database API"
+     * )
+     * @View()
+     */
+    public function getEstateTypesAction()
+    {
+        $estateTypes = $this->getDoctrine()->getRepository('DarkishCategoryBundle:EstateType')->findAll();
+
+        return new Response($this->get('jms_serializer')->serialize($estateTypes, 'json'));
+    }
+
+    /**
+     * @Post("/get_contract_types", name="_api_database_get_contract_types", defaults={"_format"= "json"},
+     * requirements={"mode": "global|record"})
+     * @ApiDoc(
+     * resource=true,
+     *  section="Database API"
+     * )
+     * @View()
+     */
+    public function getContractTypesAction()
+    {
+        $contractTypes = $this->getDoctrine()->getRepository('DarkishCategoryBundle:ContractType')->findAll();
+
+        return new Response($this->get('jms_serializer')->serialize($contractTypes, 'json'));
+    }
+
+    /**
+     * @Post("/get_automobile_types", name="_api_database_get_automobile_types", defaults={"_format"= "json"},
+     * requirements={"mode": "global|record"})
+     * @ApiDoc(
+     * resource=true,
+     *  section="Database API"
+     * )
+     * @View()
+     */
+    public function getAutomobileTypesAction()
+    {
+        $automobileTypes = $this->getDoctrine()->getRepository('DarkishCategoryBundle:AutomobileType')->findAll();
+
+        return new Response($this->get('jms_serializer')->serialize($automobileTypes, 'json'));
+    }
+
+
+    /**
+     * @Post("/get_automobile_brands", name="_api_database_get_automobile_brands", defaults={"_format"= "json"},
+     * requirements={"mode": "global|record"})
+     * @ApiDoc(
+     * resource=true,
+     *  section="Database API"
+     * )
+     * @View()
+     */
+    public function getAutomobileBrandsAction()
+    {
+        $automobileBrands = $this->getDoctrine()->getRepository('DarkishCategoryBundle:AutomobileBrand')->findAll();
+
+        return new Response($this->get('jms_serializer')->serialize($automobileBrands, 'json'));
     }
 }
