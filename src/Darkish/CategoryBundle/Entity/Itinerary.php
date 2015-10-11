af840d0654eea985c5772937bc86099191328ef7
Amir Modarresi <amir.modarresi.13@gmail.com>
2015-10-11 22:47:54 +0330

    Missing Commit Message

--- src/Darkish/CategoryBundle/Entity/Itinerary.php
+++ src/Darkish/CategoryBundle/Entity/Itinerary.php
@@ -22,6 +22,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  */
 class Itinerary implements LikableInterface, VisitableInterface, ClaimableInterface
 {
+    
     /**
      * @var integer
      *
